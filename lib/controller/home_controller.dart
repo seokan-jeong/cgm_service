59d014e25168078e926ef7466d4c31696efdf9d1
seokan-jeong <kd04199@gmail.com>
2022-11-09 14:04:42 +0900

    [Add] nfc tag

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,5 +1,32 @@
+import 'dart:convert';
+
+import 'package:flutter_nfc_kit/flutter_nfc_kit.dart';
 import 'package:get/get.dart';
+import 'package:hex/hex.dart';
+import 'package:nfc_manager/nfc_manager.dart';
+import 'package:nfc_manager/platform_tags.dart';
 
 class HomeController extends GetxController{
+  var isReadyScanning = false.obs;
 
+  @override
+  void onInit() {
+    super.onInit();
+    readNFC();
+  }
+  void readNFC() async{
+    isReadyScanning.value = false;
+    await NfcManager.instance.isAvailable();
+    NfcManager.instance.startSession(onDiscovered: (NfcTag tag) async{
+      print(tag.data);
+      final String identifier = tag.data['nfcv']['identifier']
+          .map((e) => e.toRadixString(16).padLeft(2, '0'))
+          .join('');
+      print(identifier);
+    },);
+    // var tag = await FlutterNfcKit.poll();
+    // print(jsonEncode(tag));
+    //CF46A00A00A007E0
+    isReadyScanning.value = true;
+  }
 }
--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,14 +1,30 @@
+import 'package:cgm_service/controller/home_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(HomeController());
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('CGM Service'),
         centerTitle: true,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          Center(
+              child: Obx(() => Text(
+                    controller.isReadyScanning.value
+                        ? '스캐닝 준비 완료'
+                        : '스캐닝 준비 중...',
+                    style: const TextStyle(fontSize: 16),
+                  )))
+        ],
       ),
     );
   }
